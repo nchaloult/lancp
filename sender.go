929ea61fd4d37f883984560820cc2e4c74541218
Nick Chaloult <npchaloult@gmail.com>
2021-01-03 14:24:21 -0500

    Refactor getBroadcastAddr() to accept a local address as a param

--- sender.go
+++ sender.go
@@ -16,7 +16,12 @@ func send(filePath string) error {
 	generatedPassphrase := "sender"
 
 	// Send broadcast message to find the device running in "receive mode".
-	broadcastAddr, err := getBroadcastAddr(port)
+	localAddr, err := getPreferredOutboundAddr()
+	if err != nil {
+		return fmt.Errorf("failed to get this device's local IP address: %v",
+			err)
+	}
+	broadcastAddr, err := getBroadcastAddr(localAddr, port)
 	if err != nil {
 		return fmt.Errorf("failed to get UDP broadcast address: %v", err)
 	}
@@ -81,33 +86,27 @@ func getPreferredOutboundAddr() (string, error) {
 	return preferredOutboundAddr.IP.String(), nil
 }
 
-// getBroadcastAddr finds this device's preferred outbound IPv4 address, then
+// getBroadcastAddr accepts a device's preferred outbound IPv4 address, then
 // replaces the host bytes in that address with the broadcast host (all 1s).
 // After that, it tacks on the provided port number to the address.
 //
-// For instance, if this device's local address is 192.168.0.69, then
+// For instance, if this device's preferred local address is 192.168.0.69, then
 // getBroadcastAddr will return something like 192.168.0.255:8080.
 //
 // https://stackoverflow.com/a/37382208
-func getBroadcastAddr(port int) (string, error) {
+func getBroadcastAddr(
+	preferredOutboundAddr string,
+	port int,
+) (string, error) {
 	// Validate input.
 	if port <= 1024 || port > 65535 {
 		return "", fmt.Errorf("port must be in the range (1024, 65535]."+
 			" got: %d", port)
 	}
 
-	// Get preferred outbound IP address.
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		return "", err
-	}
-	defer conn.Close()
-	preferredOutboundAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	broadcastAddr := preferredOutboundAddr.IP.String()
 	// Remove host bytes.
-	hostBytesIndex := strings.LastIndex(broadcastAddr, ".")
-	broadcastAddr = broadcastAddr[:hostBytesIndex]
+	hostBytesIndex := strings.LastIndex(preferredOutboundAddr, ".")
+	broadcastAddr := preferredOutboundAddr[:hostBytesIndex]
 	// Tack on broadcast host (all 1s) & the port number.
 	portAsStr := fmt.Sprintf(":%d", port)
 	broadcastAddr += ".255" + portAsStr
