48fdaed9aa18b2b6a7556a155aa19ff59bda74a3
Nick Chaloult <npchaloult@gmail.com>
2021-01-11 18:42:53 -0500

    Add NewSenderConfig()

--- pkg/app/config.go
+++ pkg/app/config.go
@@ -1,5 +1,11 @@
 package app
 
+import (
+	"os"
+
+	"github.com/nchaloult/lancp/pkg/net"
+)
+
 // Config stores input from command line arguments as well as configs set
 // globally. It exposes lancp's core functionality, like running in send and
 // receive mode.
@@ -14,4 +20,28 @@ type Config struct {
 	// TLSPort is the port that lancp communicates via TLS on. Stored in the
 	// format ":0000".
 	TLSPort string
+}
+
+// NewSenderConfig returns a pointer to a new Config struct intended for use by
+// lancp running in send mode.
+func NewSenderConfig(filePath string, port, tlsPort int) (*Config, error) {
+	// Make sure the file we want to send exists and we have access to it.
+	if _, err := os.Stat(filePath); err != nil {
+		return nil, err
+	}
+
+	portAsString, err := net.GetPortAsString(port)
+	if err != nil {
+		return nil, err
+	}
+	tlsPortAsString, err := net.GetPortAsString(tlsPort)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		FilePath: filePath,
+		Port:     portAsString,
+		TLSPort:  tlsPortAsString,
+	}, nil
 }
