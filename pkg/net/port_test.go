27b03d61a9e324ad9beaed79765b7e4cfe91cfa8
Nick Chaloult <npchaloult@gmail.com>
2021-01-11 18:36:18 -0500

    Add tests for GetPortAsString()

---
+++ pkg/net/port_test.go
@@ -0,0 +1,49 @@
+package net
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetPortAsString(t *testing.T) {
+	tests := []struct {
+		port        int
+		expectedRes string
+		expectedErr error
+	}{
+		// Hard-coding these port numbers in so that if the minValidPort and
+		// maxValidPort constants in port.go are changed in the future, these
+		// tests will fail.
+		{1025, ":1025", nil},
+		{65535, ":65535", nil},
+		{4242, ":4242", nil},
+		{1024, "", fmt.Errorf("port must be be in the range [%d, %d], got: %d",
+			1025, 65535, 1024)},
+		{65536, "", fmt.Errorf("port must be be in the range [%d, %d], got: %d",
+			1025, 65535, 65536)},
+		{42, "", fmt.Errorf("port must be be in the range [%d, %d], got: %d",
+			1025, 65535, 42)},
+		{8675309, "", fmt.Errorf("port must be be in the range [%d, %d], got: %d",
+			1025, 65535, 8675309)},
+	}
+
+	for _, c := range tests {
+		got, err := GetPortAsString(c.port)
+
+		if (err == nil && c.expectedErr != nil) ||
+			(err != nil && c.expectedErr == nil) ||
+			(c.expectedErr != nil && err.Error() != c.expectedErr.Error()) {
+			t.Fatalf("unexpected error, got: \"%v\"\nwant: \"%v\"",
+				err, c.expectedErr)
+		}
+		if c.expectedErr != nil && err.Error() == c.expectedErr.Error() {
+			// We expected a specific error, and we got that error. We're done
+			// with this test case.
+			continue
+		}
+
+		if got != c.expectedRes {
+			t.Fatalf("unexpected result, got: %s\nwant: %s", got, c.expectedRes)
+		}
+	}
+}
